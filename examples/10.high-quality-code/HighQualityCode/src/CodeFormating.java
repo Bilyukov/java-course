7cd53278ac819cec4a171aff19eb76b096844237
Georgi Bilyukov <georgi.bilyukov@gmail.com>
2015-01-24 19:29:48 +0200

    Added 10.high-quality-code slides and examples

---
+++ examples/10.high-quality-code/HighQualityCode/src/CodeFormating.java
@@ -0,0 +1,20 @@
+
+public class CodeFormating {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static float CalculateAverate(int[] numbers){int sum = 0;for (int i = 0;										 i < numbers.length;
+			i++) {
+			sum																 +=
+numbers[i];}float 
+result =
+
+(float)
+
+sum/numbers.length;
+		return result;	}
+
+}
---
+++ examples/10.high-quality-code/HighQualityCode/src/FunctionsExample.java
@@ -0,0 +1,18 @@
+public class FunctionsExample {
+
+	public static void main(String[] args) {
+		calculateFactorial(9);
+
+	}
+
+	public static int calculateFactorial(int number) {
+		int result = 1;
+		
+		for (int i = 1; i <= number; i++) {
+			result *= i;
+		}
+		
+		return result;
+	}
+
+}
